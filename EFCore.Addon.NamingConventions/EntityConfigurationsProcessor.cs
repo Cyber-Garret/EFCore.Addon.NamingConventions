313d17f737539f510ec1d29d7364066be6224ce4
Cyber_Garret <garret333@gmail.com>
2021-07-25 15:22:39 +0300

    Add project files.

---
+++ EFCore.Addon.NamingConventions/EntityConfigurationsProcessor.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.EntityFrameworkCore
+{
+	public class EntityConfigurationsProcessor
+	{
+		private static readonly Type ConfigurationInterfaceType;
+		private static readonly MethodInfo ApplyGenericMethod;
+
+		private readonly IReadOnlyCollection<ApplyMethodAndArguments> _applyMethodsAndArguments;
+
+		static EntityConfigurationsProcessor()
+		{
+			ConfigurationInterfaceType = typeof(IEntityTypeConfiguration<>);
+			ApplyGenericMethod = typeof(ModelBuilder).GetMethods()
+				.Single(m =>
+					m.IsGenericMethodDefinition
+					&& m.Name == nameof(ModelBuilder.ApplyConfiguration)
+					&& m.GetParameters().Length == 1
+					&& m.GetParameters()[0].ParameterType.GetGenericTypeDefinition()
+						== ConfigurationInterfaceType);
+		}
+
+		public EntityConfigurationsProcessor(string configurationsNamespace)
+		{
+			if (string.IsNullOrEmpty(configurationsNamespace))
+				throw new ArgumentNullException(nameof(configurationsNamespace));
+
+			_applyMethodsAndArguments = Assembly.GetExecutingAssembly().GetTypes()
+				.Where(t => t.IsClass && !t.IsAbstract && t.Namespace != null
+						&& t.Namespace.StartsWith(configurationsNamespace))
+				.Select(t => new
+				{
+					ConfigutationType = t,
+					ConfigutationInterfaceType = t.GetInterfaces()
+						.FirstOrDefault(it =>
+							it.IsGenericType
+							&& it.GetGenericTypeDefinition()
+							== ConfigurationInterfaceType)
+				})
+				.Where(o => o.ConfigutationInterfaceType != null)
+				.Select(o => new ApplyMethodAndArguments(
+					ApplyGenericMethod.MakeGenericMethod(
+						o.ConfigutationInterfaceType!.GetGenericArguments()[0]),
+					new[] { Activator.CreateInstance(o.ConfigutationType) }))
+				.ToArray();
+		}
+
+		public void ApplyConfigurations(ModelBuilder builder)
+		{
+			foreach (var applyMethodAndArguments in _applyMethodsAndArguments)
+				applyMethodAndArguments.Method.Invoke(builder, applyMethodAndArguments.Arguments);
+		}
+
+		private readonly struct ApplyMethodAndArguments
+		{
+			public ApplyMethodAndArguments(MethodInfo method, object?[] arguments)
+			{
+				Method = method;
+				Arguments = arguments;
+			}
+
+			public MethodInfo Method { get; }
+
+			public object?[] Arguments { get; }
+		}
+	}
+}
---
+++ EFCore.Addon.NamingConventions/Extensions/DbContextOptionsBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+
+using System;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.EntityFrameworkCore
+{
+	public static class DbContextOptionsBuilderExtensions
+	{
+		private static readonly IdColumnNameAtBeginning ExtensionInstance = new();
+
+		public static DbContextOptionsBuilder UseIdColumnNameAtBeginning(
+			this DbContextOptionsBuilder optionsBuilder)
+		{
+			if (optionsBuilder == null)
+				throw new ArgumentNullException(nameof(optionsBuilder));
+
+			((IDbContextOptionsBuilderInfrastructure)optionsBuilder)
+				.AddOrUpdateExtension(ExtensionInstance);
+			return optionsBuilder;
+		}
+	}
+}
